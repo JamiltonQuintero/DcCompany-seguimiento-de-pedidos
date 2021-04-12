f0fec7607cf2ba39c40cbadc044938d26c2ac770
JamiltonQUintero <transportador1118@gmail.com>
2021-04-11 20:46:38 -0500

    finish

--- Startup.cs
+++ Startup.cs
@@ -89,7 +89,7 @@ namespace TallerCuatro
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Admin}/{action=Dashboard}/{id?}");
             });
         }
     }
